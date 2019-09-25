2dadee292a6acb92222a8bc3c74024a38189a6fb
jacob thompson <you@example.com>
2019-09-25 13:40:22 -0500

    added framework/ and intial classes

---
+++ rockPaper/rockPaper/gameControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockPaper
+{
+    class gameControl
+    {
+        // establish wether they want to play BOT or player
+        // ask if they need to see rules 
+        //start game 
+        // hold the list of avalible throws 
+        //keep up with and compare throws  
+        //keep up with wins 
+        //if win limit is reached end game 
+        //declare winner 
+        //display wins for both players?
+    
+    }
+}
---
+++ rockPaper/rockPaper/player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockPaper
+{
+    class player
+    {
+        // should have name: Player 1,player 2,Bot.
+        // should have the ability to throw theire hand:rock,paper,scisors,lizzard,spock  
+        // 
+
+    }
+}
